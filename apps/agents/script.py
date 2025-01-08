1de9213658144ec2f06f7a62f583157e2b8fc6c1
WessimH <wessim.harmel1@gmail.com>
2025-01-08 19:50:50 +0100

    list all packages in a VM

--- apps/agents/script.py
+++ apps/agents/script.py
@@ -1,11 +1,20 @@
+"""
+Todo:
+1 - finir ce qui à faire sur le proxmox (script tourne quand le proxomox est On )
+2 -finir la route api
+3 - push dans mimir
+"""
+
 import os
 import platform
 import socket
-import psutil
-import requests
 import subprocess
 
+import psutil
+import requests
+
 API_URL = "http://localhost:3333/api/v1/servers"
+
 
 def get_server_info():
     info = {
@@ -20,6 +29,7 @@ def get_server_info():
     }
     return info
 
+
 def list_installed_packages():
     try:
         # Use dpkg-query to list installed packages
@@ -30,18 +40,18 @@ def list_installed_packages():
             text=True,
             check=True
         )
-        # Split the output into a list of packages
-        packages = result.stdout.splitlines()
+        # Split the output into a dict of packages
+        packages = {pkg: None for pkg in result.stdout.splitlines()}
         return packages
     except subprocess.CalledProcessError as e:
         print(f"Error while listing packages: {e.stderr}")
         return []
 
+#TODO :  def send_info_to_api(info):
+"""
 def send_info_to_api(info):
     response = requests.post(API_URL, json=info)
     return response.status_code
+"""
 
-if __name__ == "__main__":
-    server_info = get_server_info()
-    status = send_info_to_api(server_info)
-    print(f"Data sent to API, status code: {status}")
+
