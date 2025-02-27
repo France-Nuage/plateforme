c0576ba2983643d27f5ab2623ca2b63b05c39250
Robin Straub <rstraub@itlink.fr>
2025-02-27 16:48:16 +0100

    test: improve coverage

--- apps/control-plane/tests/e2e/api/v1/auth/login.spec.ts
+++ apps/control-plane/tests/e2e/api/v1/auth/login.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test'
 
 test.describe('/api/v1/auth/login', () => {
-  test('can authenticate with valid credentials', async ({ request }) => {
+  test('I can authenticate with valid credentials', async ({ request }) => {
     const response = await request.post('/api/v1/auth/login', {
       data: {
         email: 'admin@france-nuage.fr',
@@ -11,6 +11,7 @@ test.describe('/api/v1/auth/login', () => {
 
     expect(response.ok()).toBeTruthy()
     expect(response.status()).toBe(200)
+
     const result = await response.json()
 
     expect(Object.keys(result).sort()).toMatchObject([
@@ -22,5 +23,46 @@ test.describe('/api/v1/auth/login', () => {
       'type',
     ])
     expect(result.type.toLowerCase()).toBe('bearer')
+  })
+
+  test('I cannot authenticate with invalid credentials', async ({ request }) => {
+    const response = await request.post('/api/v1/auth/login', {
+      data: {
+        email: 'admin@france-nuage.fr',
+        password: 'an-invalid-password',
+      },
+    })
+
+    expect(response.ok()).toBeFalsy()
+    expect(response.status()).toBe(400)
+
+    const result = await response.json()
+
+    expect(result).toMatchObject({ errors: [{ message: 'Invalid user credentials' }] })
+  })
+
+  test('I am provided with validation messages on invalid content submission', async ({
+    request,
+  }) => {
+    const response = await request.post('/api/v1/auth/login')
+    const result = await response.json()
+
+    expect(response.ok()).toBeFalsy()
+    expect(response.status()).toBe(422)
+
+    expect(result).toMatchObject({
+      errors: [
+        {
+          message: 'The email field must be defined',
+          rule: 'required',
+          field: 'email',
+        },
+        {
+          message: 'The password field must be defined',
+          rule: 'required',
+          field: 'password',
+        },
+      ],
+    })
   })
 })
