fb0b81c548d14a5e1c7f82afbb31b6c2e87a2b1e
Alexandre Cailler <alex02.cailler@gmail.com>
2025-03-13 09:53:39 +0100

    fix: review

--- apps/tests/playwright.config.ts
+++ apps/tests/playwright.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: process.env.BASE_URL || "http://platform:3000",
+    baseURL: process.env.BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
@@ -41,29 +41,10 @@ export default defineConfig({
       testDir: "./platform",
       use: { ...devices["Desktop Chrome"] },
     },
-    //
-    // {
-    //   name: "firefox",
-    //   testDir: "./platform",
-    //   use: { ...devices["Desktop Firefox"] },
-    // },
-    //
-    // {
-    //   name: "webkit",
-    //   testDir: "./platform",
-    //   use: { ...devices["Desktop Safari"] },
-    // },
     {
       name: "api",
       testDir: "./api",
       use: { ...devices["Desktop Firefox"] },
     },
   ],
-
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run dev',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
 });
