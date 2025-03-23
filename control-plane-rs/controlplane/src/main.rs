a25120e4bdab01466b4d30949555d1ebfc60a600
Robin <robin@straub.pro>
2025-03-23 11:12:00 +0100

    build: configure the controlplane with environment variables

--- control-plane-rs/controlplane/src/main.rs
+++ control-plane-rs/controlplane/src/main.rs
@@ -2,7 +2,11 @@ use controlplane::server::{Server, ServerConfig};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let config = ServerConfig::new(String::from("https://pve-poc01-internal.france-nuage.fr"));
+    let config = ServerConfig {
+        addr: Some(String::from("[::1]:80")),
+        api_url: std::env::var("PROXMOX_API_URL").expect("PROXMOX_API_URL not set"),
+        authentication_header: std::env::var("PROXMOX_AUTHORIZATION_HEADER").ok(),
+    };
     Server::new(config).await?.serve().await?;
     Ok(())
 }
