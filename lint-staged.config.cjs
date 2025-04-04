4c2814f7ea9ec464052a6f2e7036b2c89919d688
Alexandre Cailler <alex02.cailler@gmail.com>
2025-04-04 14:47:37 +0200

    feat: add pnpm run lint

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,6 +1,7 @@
 module.exports = {
   "controlplane/**/*.rs": [
     "cd controlplane && cargo fmt --",
-    "cd controlplane && cargo clippy -- -D warnings"
+    "cd controlplane && cargo clippy -- -D warnings",
+    "cd webui && pnpm run lint"
   ],
 };
