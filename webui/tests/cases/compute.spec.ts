f7640dcccab241db74b640397027a22eb65f21fb
Robin <robin@straub.pro>
2025-04-05 18:07:58 +0200

    chore: apply coderabbit suggestions

--- webui/tests/cases/compute.spec.ts
+++
@@ -1,8 +0,0 @@
-import { test } from "../base";
-
-test.describe('Compute', () => {
-  test('I can access the compute page', async ({ page, pages }) => {
-    await pages.compute.goto();
-    await pages.compute.assertLocation();
-  });
-});
