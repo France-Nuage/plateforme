d76e1231ec8c00c80ff52eec47d51b6f4b7ded76
Robin <robin@straub.pro>
2025-04-21 18:44:36 +0200

    chore: move rust codestyle checks to lint-staged

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -1,7 +1,8 @@
 module.exports = {
   "controlplane/**/*.rs": [
-    "cd controlplane && cargo fmt --",
-    "cd controlplane && cargo clippy -- -D warnings",
+    "cargo fmt --all --manifest-path=controlplane/Cargo.toml",
+    "cargo clippy --fix --allow-dirty --manifest-path=controlplane/Cargo.toml"
+
   ],
   "console/**/*.(ts|js|vue|json|css|scss|html)": [
     "cd console && pnpm run lint",
