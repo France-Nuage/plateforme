44ba82cc14c27363de4905ef0c310870835232d3
Robin <robin@straub.pro>
2025-05-02 11:12:41 +0200

    feat: add working plasmic app with fixtures and codegen

---
+++ plasmic-console/fixtures/index.ts
@@ -0,0 +1,2 @@
+export * from "./hypervisor";
+export * from "./instance";
---
+++ plasmic-console/pages/_app.tsx
@@ -0,0 +1,18 @@
+import { wrapper } from '@/store';
+import '@/styles/globals.css'
+import { PlasmicRootProvider } from "@plasmicapp/react-web";
+import type { AppProps } from "next/app";
+import Head from "next/head";
+import Link from "next/link";
+import { Provider } from 'react-redux';
+
+export default function MyApp({ Component, ...rest }: AppProps) {
+  const { store, props } = wrapper.useWrappedStore(rest);
+  return (
+    <Provider store={store}>
+      <PlasmicRootProvider Head={Head} Link={Link}>
+        <Component {...props.pageProps} />
+      </PlasmicRootProvider>
+    </Provider>
+  );
+}
