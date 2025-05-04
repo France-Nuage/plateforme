3537f49febbee0d278649131e3abb8b154df0e33
Robin <robin@straub.pro>
2025-05-04 09:53:21 +0200

    fix: codestyle

--- lint-staged.config.cjs
+++ lint-staged.config.cjs
@@ -3,7 +3,7 @@ module.exports = {
     () => "cargo fmt --all --manifest-path=controlplane/Cargo.toml",
     () => "cargo clippy --fix --allow-dirty --manifest-path=controlplane/Cargo.toml"
   ],
-  "console/**/*.(ts|js|vue|json|css|scss|html)": [
+  "console/**/*": [
     () => "cd console && npm run lint",
     () => "cd console && npx prettier --write"
   ]
