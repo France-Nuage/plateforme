bec80702ae2cfa67272914ffd678e4bf1ace856b
Robin <robin@straub.pro>
2025-05-30 22:32:38 +0200

    chore: remove previous nextjs console

--- next-console/services/hypervisor.interface.ts
+++
@@ -1,6 +0,0 @@
-import { Hypervisor, HypervisorFormValue } from "@/types";
-
-export interface HypervisorService {
-  list: () => Promise<Hypervisor[]>;
-  register: (value: HypervisorFormValue) => Promise<Hypervisor>;
-}
--- next-console/services/instance.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Instance, InstanceFormValue } from "@/types";
-
-export interface InstanceService {
-  /** Lists the available instances. */
-  list: () => Promise<Instance[]>;
-
-  /** Create a new instance. */
-  create: (data: InstanceFormValue) => Promise<Instance>;
-}
